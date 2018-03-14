5c166c912ebe570493346d91aa4e5e78e9033bf3
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-03-14 20:24:32 +0100

    change components app

--- components/App.js
+++ components/App.js
@@ -10,23 +10,6 @@ App = React.createClass({
     };
 },
 
-    getGif: function(searchingText, callback) {  // 1.
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();  // 3.
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data; // 4.
-                var gif = {  // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);  // 6.
-            }
-        };
-        xhr.send();
-    },
-
     getGif: function() {
         return new Promise (
             function (resolve, reject) {
@@ -46,10 +29,13 @@ App = React.createClass({
 
             }
         };
-        getGif()
-        then(xhr.send());
+
+    
     }
 
+getGif();
+.then(gif);
+.catch(error);
 
 
 
